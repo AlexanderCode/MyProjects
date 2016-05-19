c81b0b3de031cf8ce5e6033bc9620ab80d5d3cbc
Alexander Ross <alexandersmithross@gmail.com>
2016-05-19 12:00:45 -0700

    update name game with clean method

--- Name_Game.rb
+++ Name_Game.rb
@@ -1,37 +1,43 @@
 puts "what is your name?"
 name=gets.chomp
 
-if name=='Lily' 
+answer = case
+when name == 'Lily' 
 
-  then puts "Wow I am so happy to meet you "+name+"! You are Alexanders favorite wife! He told me
+  "Wow I am so happy to meet you "+ name +"! You are Alexanders favorite wife! He told me
 so himself! We talk alot about you becuase your husband just cant stop telling
 me how much he loves you.  Also he told me not to make that joke about the favorite wife
 but I couldnt resist!"
-end 
+ 
 
 
-if name == 'Alexander'
+when name == 'Alexander'
 
-  then puts "Hi " + name + " the Great, I have heard you are the master of my universe! All this time 
+  "Hi " + name + " the Great, I have heard you are the master of my universe! All this time 
 we have spent together where I watched you flounder around like you knew nothing and here 
 you are master of my universe and I didn't even know!"
-end
-if name == 'Alex'
-  then puts "Hi " + name + ", I have heard you are the master of my universe! All this time we have spent 
+
+when name == 'Alex'
+  "Hi " + name + ", I have heard you are the master of my universe! All this time we have spent 
 together where I watched you flounder around like you knew nothing and here you are master
 of my universe and I didn't even know!"
-end
-if name == 'Brendan'
 
-  then puts "Hello " + name + " it is very nice to meet you. I have heard alot about you. Your dad told me 
-just how much he loves you and how he is so proud of you."
-end
-if name == 'Bren'
-  then puts "Hello " + name + " it is very nice to meet you. I have heard alot about you. Your dad told me 
-just how much he loves you and how he is so proud of you."
-end
+when name == 'Brendan'
 
-if name == 'Autumn'
-  then puts "Hello " + name + " I am so impressed you know your name before you could talk! Wait a second!
+  "Hello " + name + " it is very nice to meet you. I have heard alot about you. Your dad told me 
+just how much he loves you and how he is so proud of you."
+
+when name == 'Bren'
+  "Hello " + name + " it is very nice to meet you. I have heard alot about you. Your dad told me 
+just how much he loves you and how he is so proud of you."
+
+
+when name == 'Autumn'
+  "Hello " + name + " I am so impressed you know your name before you could talk! Wait a second!
 Who is this! This can not be Autumn since I was told she is not even 1 year old yet!"
+
+else
+  "Hi " + name +"!  It is so very nice to meet you!  I love meeting new people."
 end
+
+puts answer
